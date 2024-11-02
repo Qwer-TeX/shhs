c4cfb46b7e82f918c8d0f8312359716e8d7d21e7
Qwer-TeX <128016252+Qwer-TeX@users.noreply.github.com>
2024-11-02 02:46:06 -0400

    Update hash.h
    Add blake2 support

--- hash.h
+++ hash.h
@@ -5,5 +5,6 @@ void calculate_md5(const char *filename);
 void calculate_sha1(const char *filename);
 void calculate_sha256(const char *filename);
 void calculate_sha512(const char *filename);
+void calculate_blake2(const char *filename);
 
 #endif // HASH_H
