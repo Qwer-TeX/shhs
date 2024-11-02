ffc7b7311b49c410b32e7203e251619bba3438ba
Qwer-TeX <128016252+Qwer-TeX@users.noreply.github.com>
2024-11-02 02:18:30 -0400

    Create SHHS.c
    Initial commit

---
+++ SHHS.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const char *filename = argv[1];
+
+    calculate_md5(filename);
+    calculate_sha1(filename);
+    calculate_sha256(filename);
+    calculate_sha512(filename);
+
+    return EXIT_SUCCESS;
+}
